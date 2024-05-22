cc270f722f6cf097b59022e3e9a74275c7a195fe
w4t3rcs <uimanovmaks@gmail.com>
2024-05-22 22:03:21 +0200

    Initialize database schema and add Flyway dependency
    A new 'users' table has been created in the database schema by adding a SQL migration script. Additionally, the 'ddl-auto' setting in 'application-dev.yaml' has been set to 'none', disabling automatic schema updates. The Flyway dependency has been added to the project to manage future database migrations.

---
+++ src/main/resources/db/migration/V1__initializing_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(64) NOT NULL,
+    last_name VARCHAR(64) NOT NULL,
+    email VARCHAR(128) UNIQUE NOT NULL,
+    birth_date DATE NOT NULL,
+    address VARCHAR(128),
+    phone_number VARCHAR(16)
+)
