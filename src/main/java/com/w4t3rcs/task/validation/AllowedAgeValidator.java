2e98daa4a0da7bf8a0a6ec8ebb5c995dab5d8360
w4t3rcs <uimanovmaks@gmail.com>
2024-05-22 22:02:51 +0200

    Remove unnecessary annotations and override in AllowedAgeValidator
    Annotations @Data and @Component and the super calls to initialize method in AllowedAgeValidator were deemed unnecessary. Their removal simplifies the code and maintains the functionality of the class, as they did not provide substantial additional value.

--- src/main/java/com/w4t3rcs/task/validation/AllowedAgeValidator.java
+++ src/main/java/com/w4t3rcs/task/validation/AllowedAgeValidator.java
@@ -5,15 +5,11 @@ import com.w4t3rcs.task.exception.InvalidAgeException;
 import com.w4t3rcs.task.validation.annotation.AllowedAge;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import lombok.Data;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
-@Data
-@Component
 public class AllowedAgeValidator implements ConstraintValidator<AllowedAge, LocalDate> {
     private static final String ALLOWED_AGE_PROPERTY = "ALLOWED_AGE";
     private LocalDate minimumDate;
@@ -21,7 +17,6 @@ public class AllowedAgeValidator implements ConstraintValidator<AllowedAge, Loca
 
     @Override
     public void initialize(AllowedAge constraintAnnotation) {
-        ConstraintValidator.super.initialize(constraintAnnotation);
         Environment environment = ApplicationContextProvider.getEnvironment();
         String ageProperty = Objects.requireNonNull(environment.getProperty(ALLOWED_AGE_PROPERTY));
         int allowedAge = Integer.parseInt(ageProperty);
